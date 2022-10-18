4fe4a08adc2362b9e80bdd35b2f59f463eb994d3
Antoinette Akinyi <antoinette@kisha.mshome.net>
2022-10-18 08:51:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener{addNickname(it)}
+    }
+    private fun addNickname(view:View){
+        val editText = findViewById<EditText>(R.id.nick_name)
+        val nicknameTextView = findViewById<TextView>(R.id.display)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+
+    }
+}
